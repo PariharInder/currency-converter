df7a4e4ef47b211c1fd19dd76590677e2a5410c7
PariharInder <2002inderjitsingh@gmail.com>
2023-02-24 14:11:10 -0500

    added ability to get the avaiblable currencies from api

--- src/components/CurrencyOutput.tsx
+++ src/components/CurrencyOutput.tsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from 'react'
+import { GetAllCurrencies } from '../services/ConvertCurrencies'
 import '../styles/main.css'
 
 export const CurrencyOutput = () => {
+    const [rates, setRates] = useState<string[]>([])
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            const result = await GetAllCurrencies()
+            if(result!=undefined) setRates(result)
+        }
+        fetchData()
+    }, [])
+    
     return (
         <div className="text-center">
             <div className="m-4">
             <label>Select the currency: </label>
             <select name="currency-1" id="">
                 <option value="USD">USD</option>
+                {rates!= undefined && rates?.map((rate, index) => (
+                    <option value={rate}>{rate}</option>
+                ))}
             </select>
             </div>
             <div>
--- src/services/ConvertCurrencies.tsx
+++ src/services/ConvertCurrencies.tsx
@@ -1,3 +1,20 @@
+import axios from "axios"
+
+const APP_ID = '51afea30f6334ddea436cbf6b5ed95f9'
+const API_URL = `https://openexchangerates.org/api/latest.json?app_id=${APP_ID}`
+
 export const ConvertCurrencies = () => {
 
+}
+
+export const GetAllCurrencies = () => {
+return axios.get(API_URL)
+  .then(response => {
+    const rates = response.data.rates
+    const currencies = Object.keys(rates)
+    return currencies
+  })
+  .catch(error => {
+    console.error(error)
+  })
 }
